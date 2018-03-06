fa5156607b872aeb0e369ddbf33b03bc8962d759
Anna Nikolaeva <amnik21@yandex.ru>
2018-03-06 22:18:53 +0300

    Добавила объект Представление: в нем три метода отображения сообщения, попадания и промаха игрока.

---
+++ chapter 8 application/battleship.js
@@ -0,0 +1,12 @@
+var view = {
+    displayMessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        var cell = document.getElementById(location);
+    },
+    displayMiss: function(location) {
+
+    }
+};
