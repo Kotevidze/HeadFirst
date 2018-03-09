2ae7612078576c2fd2afbe69e89e12e41b2080c9
Anna Nikolaeva <amnik21@yandex.ru>
2018-03-09 13:30:54 +0300

    - Отобразила ходы игрока на игровом поле в рамках упражнения со стр. 363 - Тест на проверку задания со стр. 364

--- chapter 8 application/battleship.js
+++ chapter 8 application/battleship.js
@@ -5,8 +5,29 @@ var view = {
     },
     displayHit: function(location) {
         var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit")
     },
     displayMiss: function(location) {
-
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss")
     }
 };
+
+view.displayHit("06");
+view.displayMiss("13");
+view.displayHit("24");
+view.displayMiss("31");
+view.displayHit("10");
+view.displayHit("34");
+view.displayMiss("50");
+view.displayMiss("01");
+view.displayHit("26");
+view.displayHit("11");
+view.displayHit("12");
+view.displayHit("44");
+view.displayHit("16");
+view.displayMessage("Tap tap, is this thing in?");
+
+var ships = [{location: ["06", "16", "26"], hits: ["hit", "hit", "hit"]},
+            {location: ["24", "34", "44"], hits: ["hit", "hit", "hit"]},
+            {location: ["10", "11", "12"], hits: ["hit", "hit", "hit"]}];
--- example.js
+++ example.js
@@ -1,8 +1,23 @@
-function doIt(param) {
-    param = 3;
-    console.log(param + "Gamarjoba");
+//var ship1 = { locations: ["31", "41", "51"], hits: ["", "", ""]};
+//var ship2 = { locations: ["14", "24", "34"], hits: ["", "", ""]};
+//var ship3 = { locations: ["00", "01", "02"], hits: ["", "", ""]};
+
+var ships = [{ locations: ["31", "41", "51"], hits: ["", "", ""]},
+             { locations: ["14", "24", "34"], hits: ["", "hit", ""]},
+             { locations: ["00", "01", "02"], hits: ["hit", "", ""]}];
+
+var ship2 = ships[1];
+var locations = ship2.locations;
+console.log("Location is " + locations[1]);
+
+var ship3 = ships[2];
+var hits = ship3.hits;
+if ( hits[0] === "hit") {
+    console.log("Ouch, hit on third ship at location one");
 }
 
-var test = 1;
-doIt(test);
-console.log(test);
+var ship1 = ships[0];
+var hits = ship1.hits;
+hits[2] = "hit";
+
+console.log(ship1.hits);
