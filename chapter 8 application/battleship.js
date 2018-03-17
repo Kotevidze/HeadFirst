34cf53f6db819684d6073777168d2b85bfbfe5a9
Anna Nikolaeva <amnik21@yandex.ru>
2018-03-17 20:28:15 +0300

    Добавила объект Модель с методами fire и isSunk (стр. 365 - 371). Реализовала функцию с итеративным процессом (урок 9 на Хэкслете-Введение в программирование).

--- chapter 8 application/battleship.js
+++ chapter 8 application/battleship.js
@@ -13,21 +13,63 @@ var view = {
     }
 };
 
-view.displayHit("06");
-view.displayMiss("13");
-view.displayHit("24");
-view.displayMiss("31");
-view.displayHit("10");
+view.displayMiss("00");
 view.displayHit("34");
-view.displayMiss("50");
-view.displayMiss("01");
-view.displayHit("26");
-view.displayHit("11");
+view.displayMiss("55");
 view.displayHit("12");
-view.displayHit("44");
-view.displayHit("16");
+view.displayHit("25");
+view.displayMiss("26");
+
 view.displayMessage("Tap tap, is this thing in?");
 
-var ships = [{location: ["06", "16", "26"], hits: ["hit", "hit", "hit"]},
-            {location: ["24", "34", "44"], hits: ["hit", "hit", "hit"]},
-            {location: ["10", "11", "12"], hits: ["hit", "hit", "hit"]}];
+/*var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+    ships = [
+        {location: ["06", "16", "26"], hits: ["hit", "hit", "hit"]},
+        {location: ["24", "34", "44"], hits: ["hit", "hit", "hit"]},
+        {location: ["10", "11", "12"], hits: ["hit", "hit", "hit"]}
+    ],
+    fire: function(guess) {
+        for (var i = 0; i < this.numShips; i++) {
+            var ship = this.ships[i];
+            // location = ship.location; Временная переменная, используется для промежуточного хранения массива ship.locations/
+            var index = ship.locations.indexOf(guess) // Метод indexOf ищет в масиве указанное значение и возвращает его индекс (или -1, если значения нет в массиве)
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                view.displayHit(guess);
+                view.displayMessage("HIT!");
+                if (this.isSunk(ship)) {
+                    view.displayMessage("You sank my battleship!");
+                    this.shipsSunk++;
+                }
+                return true;
+            }
+        }
+        view.displayMiss(guess);
+        view.displayMessage("You missed.");
+        return false;
+    },
+
+    isSunk: function(ship) {
+        for (var i = 0; i < this.shipLength; i++) {
+            if (ship.hits[i] !== "hit") {
+                return false;
+            }
+        }
+        return true;SS
+    }S
+}
+*/
+model.fire("53");
+model.fire("06");
+model.fire("16");
+model.fire("26");
+model.fire("34");
+model.fire("24");
+model.fire("44");
+model.fire("12");
+model.fire("11");
+model.fire("10");
--- example.js
+++ example.js
@@ -1,23 +1,12 @@
-//var ship1 = { locations: ["31", "41", "51"], hits: ["", "", ""]};
-//var ship2 = { locations: ["14", "24", "34"], hits: ["", "", ""]};
-//var ship3 = { locations: ["00", "01", "02"], hits: ["", "", ""]};
+const factorial = (n) => {
+    const iter = (counter, acc) => {
+      if (counter === 1) {
+        return acc;
+      }
+      return iter(counter - 1, counter * acc);
+    };
+  
+    return iter(n, 1);
+  };
 
-var ships = [{ locations: ["31", "41", "51"], hits: ["", "", ""]},
-             { locations: ["14", "24", "34"], hits: ["", "hit", ""]},
-             { locations: ["00", "01", "02"], hits: ["hit", "", ""]}];
-
-var ship2 = ships[1];
-var locations = ship2.locations;
-console.log("Location is " + locations[1]);
-
-var ship3 = ships[2];
-var hits = ship3.hits;
-if ( hits[0] === "hit") {
-    console.log("Ouch, hit on third ship at location one");
-}
-
-var ship1 = ships[0];
-var hits = ship1.hits;
-hits[2] = "hit";
-
-console.log(ship1.hits);
+console.log(factorial(3));
