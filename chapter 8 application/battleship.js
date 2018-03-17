71e76c482f89174b711e9656459dc12815b2183c
Anna Nikolaeva <amnik21@yandex.ru>
2018-03-17 21:45:42 +0300

    Исправила синтаксические ошибки, раскомментировала объект Model

--- chapter 8 application/battleship.js
+++ chapter 8 application/battleship.js
@@ -22,21 +22,21 @@ view.displayMiss("26");
 
 view.displayMessage("Tap tap, is this thing in?");
 
-/*var model = {
+var model = {
     boardSize: 7,
     numShips: 3,
     shipLength: 3,
     shipsSunk: 0,
-    ships = [
-        {location: ["06", "16", "26"], hits: ["hit", "hit", "hit"]},
-        {location: ["24", "34", "44"], hits: ["hit", "hit", "hit"]},
-        {location: ["10", "11", "12"], hits: ["hit", "hit", "hit"]}
+    ships: [
+        {location: ["06", "16", "26"], hits: ["", "", ""]},
+        {location: ["24", "34", "44"], hits: ["", "", ""]},
+        {location: ["10", "11", "12"], hits: ["", "", ""]}
     ],
     fire: function(guess) {
         for (var i = 0; i < this.numShips; i++) {
             var ship = this.ships[i];
             // location = ship.location; Временная переменная, используется для промежуточного хранения массива ship.locations/
-            var index = ship.locations.indexOf(guess) // Метод indexOf ищет в масиве указанное значение и возвращает его индекс (или -1, если значения нет в массиве)
+            var index = ship.location.indexOf(guess); // Метод indexOf ищет в масиве указанное значение и возвращает его индекс (или -1, если значения нет в массиве)
             if (index >= 0) {
                 ship.hits[index] = "hit";
                 view.displayHit(guess);
@@ -59,10 +59,10 @@ view.displayMessage("Tap tap, is this thing in?");
                 return false;
             }
         }
-        return true;SS
-    }S
+        return true;
+    }
 }
-*/
+
 model.fire("53");
 model.fire("06");
 model.fire("16");
