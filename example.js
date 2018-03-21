e98ca3e8684cc254dfad021f143a37a848289095
Anna Nikolaeva <amnik21@yandex.ru>
2018-03-21 20:12:27 +0300

    Сделала задание на Hexlet: курс Введение в программирование, #10

--- example.js
+++ example.js
@@ -1,12 +1,27 @@
-const factorial = (n) => {
-    const iter = (counter, acc) => {
-      if (counter === 1) {
-        return acc;
-      }
-      return iter(counter - 1, counter * acc);
-    };
-  
-    return iter(n, 1);
+const smallestDivisor = (num) => {
+  const iter = (acc) => {
+    if (acc > num/2) {
+      return num;
+    }
+    if (num % acc === 0) {
+      return acc;
+    }
+    return iter(acc + 1);
   };
+  return iter(2);
+};
 
-console.log(factorial(3));
+console.log(smallestDivisor(7));
+
+var smallestDivisor2 = function(num) {
+  var acc = 2;
+  while (acc < num / 2) {
+    if (num % acc === 0) {
+      return acc;
+    }
+    acc++;
+  } 
+  return num; 
+}
+
+console.log(smallestDivisor2(15));
